652ce25e6bb64f80717902467072b41217d554a0
Claire-web <57494370+Claire-web@users.noreply.github.com>
2020-01-16 13:49:44 +0800

    update password-retry file

--- password-retry.py
+++ password-retry.py
@@ -1,12 +1,15 @@
 password = 'a123456'
 p = 3 # 剩餘機會
-while True:
+while p > 0:
+    p = p - 1
     pwd = input('請輸入密碼: ')
     if pwd == password: 
         print('登入成功!')
         break # 逃出迴圈
     else:
-        p = p - 1
-        print('密碼錯誤! 還有' , p, '次機會')
-        if p == 0:
-            break
+        print('密碼錯誤!')
+        if p > 0:
+            print('還有' , p, '次機會')
+        else:
+            print('帳號已鎖')
+        
